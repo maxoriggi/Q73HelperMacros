8e52085612cbaf5c54158c1c9238ca74eef1d59a
Massimiliano Origgi <max@q73.it>
2024-09-18 09:12:08 +0200

    Updated URL for swift-syntax.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "509.0.0"),
         .package(url: "https://github.com/sindresorhus/Defaults.git", from: "8.2.0")
     ],
     targets: [
