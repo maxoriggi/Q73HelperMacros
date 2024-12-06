120546d799a9d7516abb5b51ab16696b4de5a943
Massimiliano Origgi <max@q73.it>
2024-12-06 13:27:46 +0100

    Updated Defaults to version 9.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/sindresorhus/Defaults.git", from: "8.2.0")
+        .package(url: "https://github.com/sindresorhus/Defaults.git", from: "9.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
